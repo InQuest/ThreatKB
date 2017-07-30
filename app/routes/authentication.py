12c5ab83f0ce15b451aaefdc6e8b6ce92cf2a225
danny <danny@vantagepoint.co>
2017-07-30 09:59:26 -0500

    Use flask-login to login user.

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -1,17 +1,19 @@
 from app import app, bcrypt
 from app.models.authentication import KBUser
 from flask import request, jsonify, session
-
+from flask.ext.login import current_user
+import flask_login
 
 @app.route('/InquestKB/login', methods=['POST'])
 def login():
     app.logger.info("login called with payload: '%s'" % (request.data))
     json_data = request.json
-    user = KBUser.query.filter_by(email=json_data['email']).first()
+    user = KBUser.query.filter_by(email=json_data.get('email', None)).first()
     app.logger.info("user is '%s'" % (user))
     if user and bcrypt.check_password_hash(user.password, json_data['password']):
         session['logged_in'] = True
         status = True
+        flask_login.login_user(user)
     else:
         status = False
     return jsonify({'result': status})
@@ -20,12 +22,13 @@ def login():
 @app.route('/InquestKB/logout')
 def logout():
     session.pop('logged_in', None)
+    flask_login.logout_user()
     return jsonify({'result': 'success'})
 
 
 @app.route('/InquestKB/status')
 def status():
-    app.logger.info("session is: '%s'" % (session))
+    app.logger.debug("status current_user is '%s'" % (str(current_user)))
     if session.get('logged_in'):
         if session['logged_in']:
             return jsonify({'status': True})
