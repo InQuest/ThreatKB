6e55473859a5b109a25b51250244d1e4197adb78
danny <danny@vantagepoint.co>
2017-07-30 10:01:09 -0500

    Back end for comments functionality.

---
+++ app/routes/comments.py
@@ -0,0 +1,53 @@
+from app import app, db
+from app.models import comments
+from flask import abort, jsonify, request
+import json
+from flask.ext.login import login_required, current_user
+
+
+@app.route('/InquestKB/comments', methods=['GET'])
+def get_all_comments():
+    entity_type = request.args.get("entity_type", None)
+    entity_id = request.args.get("entity_id", None)
+    entities = comments.Comments.query
+    if entity_type:
+        entities = entities.filter_by(entity_type=entity_type)
+    if entity_id:
+        entities = entities.filter_by(entity_id=entity_id)
+
+    entities = entities.all()
+    return json.dumps([entity.to_dict() for entity in entities])
+
+
+@app.route('/InquestKB/comments/<int:id>', methods=['GET'])
+@login_required
+def get_comments(id):
+    entity = comments.Comments.query.get(id)
+    if not entity:
+        abort(404)
+    return jsonify(entity.to_dict())
+
+
+@app.route('/InquestKB/comments', methods=['POST'])
+@login_required
+def create_comments():
+    entity = comments.Comments(
+        comment=request.json['comment']
+        , entity_type=request.json['entity_type']
+        , entity_id=request.json['entity_id']
+        , user_id=current_user.id
+    )
+    db.session.add(entity)
+    db.session.commit()
+    return jsonify(entity.to_dict()), 201
+
+
+@app.route('/InquestKB/comments/<int:id>', methods=['DELETE'])
+@login_required
+def delete_comments(id):
+    entity = comments.Comments.query.get(id)
+    if not entity:
+        abort(404)
+    db.session.delete(entity)
+    db.session.commit()
+    return '', 204
