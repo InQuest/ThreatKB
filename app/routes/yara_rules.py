f6cae0262e30079c72c22a220c9af3031b8040a1
danny <danny@vantagepoint.co>
2017-08-07 00:08:17 -0500

    Saving work.

--- app/routes/yara_rules.py
+++ app/routes/yara_rules.py
@@ -4,6 +4,7 @@ from flask import abort, jsonify, request
 from flask.ext.login import current_user, login_required
 from app.routes.tags_mapping import create_tags_mapping, delete_tags_mapping
 import json
+import datetime
 
 
 @app.route('/InquestKB/yara_rules', methods=['GET'])
@@ -55,9 +56,12 @@ def create_yara_rule():
 @app.route('/InquestKB/yara_rules/<int:id>', methods=['PUT'])
 @login_required
 def update_yara_rule(id):
-    entity = yara_rule.Yara_rule.query.get(id)
-    if not entity:
+    do_not_bump_revision = request.args.get("do_not_bump_revision", False)
+
+    old_entity = yara_rule.Yara_rule.query.get(id)
+    if not old_entity:
         abort(404)
+
     entity = yara_rule.Yara_rule(
         state=request.json['state'],
         name=request.json['name'],
@@ -75,10 +79,17 @@ def update_yara_rule(id):
         condition=request.json['condition'],
         strings=request.json['strings'],
         id=id,
-        modified_user_id=current_user.id
+        modified_user_id=current_user.id,
+        revision=old_entity.revision if do_not_bump_revision else old_entity.revision + 1
     )
     db.session.merge(entity)
     db.session.commit()
+
+    if not do_not_bump_revision:
+        db.session.add(yara_rule.Yara_rule_history(date_created=old_entity.date_created, revision=old_entity.revision,
+                                                   rule_json=json.dumps(old_entity.to_revision_dict()),
+                                                   user_id=current_user.id,
+                                                   yara_rule_id=old_entity.id))
 
     create_tags_mapping(entity.__tablename__, entity.id, request.json['addedTags'])
     delete_tags_mapping(entity.__tablename__, entity.id, request.json['removedTags'])
--- app/static/js/yara_rule/yara_rule-controller.js
+++ app/static/js/yara_rule/yara_rule-controller.js
@@ -88,6 +88,7 @@ angular.module('InquestKB')
             $scope.Comments = Comments;
 
             $scope.cfg_states = Cfg_states.query();
+            $scope.do_not_bump_revision = false;
 
             $scope.print_comment = function (comment) {
                 return comment.comment.replace(/(?:\r\n|\r|\n)/g, "<BR>");
@@ -101,7 +102,8 @@ angular.module('InquestKB')
                 $scope.Comments.resource.save({
                     comment: $scope.yara_rule.new_comment,
                     entity_type: Comments.ENTITY_MAPPING.SIGNATURE,
-                    entity_id: id
+                    entity_id: id,
+                    do_not_bump_revision: $scope.do_not_bump_revision
                 }, function () {
                     $scope.yara_rule.new_comment = "";
                     $scope.yara_rule.comments = $scope.Comments.resource.query({
