b2a3805f0ee7979eb6a1c8209fed424aa4958326
danny <danny@vantagepoint.co>
2017-08-29 11:31:20 -0500

    is_release_state functionality for states

--- app/models/cfg_states.py
+++ app/models/cfg_states.py
@@ -1,5 +1,5 @@
 from app import db
-
+from sqlalchemy.event import listens_for
 
 class Cfg_states(db.Model):
     __tablename__ = "cfg_states"
@@ -7,12 +7,26 @@ class Cfg_states(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     state = db.Column(db.String(32))
+    is_release_state = db.Column(db.Integer, default=0)
 
     def to_dict(self):
         return dict(
             state=self.state,
-            id=self.id
+            id=self.id,
+            is_release_state=self.is_release_state
         )
 
     def __repr__(self):
         return '<Cfg_states %r>' % (self.id)
+
+
+@listens_for(Cfg_states, "before_insert")
+def generate_signature_id(mapper, connect, target):
+    if target.is_release_state > 0:
+        Cfg_states.query.update(dict(is_release_state=0))
+
+
+@listens_for(Cfg_states, "before_update")
+def generate_signature_id(mapper, connect, target):
+    if target.is_release_state > 0:
+        Cfg_states.query.update(dict(is_release_state=0))
