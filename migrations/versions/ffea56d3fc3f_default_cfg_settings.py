6a1a5eb8307b80535ec2b29091a7e504e3aed526
danny <danny@vantagepoint.co>
2017-09-17 12:46:07 -0500

    .

--- migrations/versions/ffea56d3fc3f_default_cfg_settings.py
+++ migrations/versions/ffea56d3fc3f_default_cfg_settings.py
@@ -8,6 +8,7 @@ Create Date: 2017-09-17 11:16:07.252228
 from alembic import op
 from app.models import cfg_settings
 import sqlalchemy as sa
+import datetime
 
 # revision identifiers, used by Alembic.
 revision = 'ffea56d3fc3f'
@@ -17,19 +18,32 @@ depends_on = None
 
 
 def upgrade():
+    date_created = datetime.datetime.now().isoformat()
+    date_modified = datetime.datetime.now().isoformat()
+
     op.bulk_insert(
         cfg_settings.Cfg_settings.__table__,
         [
-            {"key": "NAV_IMAGE", "value": "inquest_logo.svg", "public": True},
-            {"key": "YARA_PATH", "value": "/usr/local/bin/yara", "public": True},
-            {"key": "CLEAN_FILES_CORPUS_DIRECTORY", "value": "files/clean/", "public": True},
-            {"key": "ATTACHED_FILES_CORPUS_DIRECTORY", "value": "files/attached/", "public": True},
-            {"key": "ATTACHED_FILES_PREPROCESSOR", "value": "", "public": True},
-            {"key": "REDIS_BROKER_URL", "value": "redis://localhost:6379/0", "public": False},
-            {"key": "REDIS_TASK_SERIALIZER", "value": "json", "public": False},
-            {"key": "REDIS_RESULT_SERIALIZER", "value": "json", "public": False},
-            {"key": "REDIS_ACCEPT_CONTENT", "value": "[\"json\"]", "public": False},
-            {"key": "MAX_MILLIS_PER_FILE_THRESHOLD", "value": "3.0", "public": False}
+            {"key": "NAV_IMAGE", "value": "inquest_logo.svg", "public": True, "date_created": date_created,
+             "date_modified": date_modified},
+            {"key": "YARA_PATH", "value": "/usr/local/bin/yara", "public": True, "date_created": date_created,
+             "date_modified": date_modified},
+            {"key": "CLEAN_FILES_CORPUS_DIRECTORY", "value": "files/clean/", "public": True,
+             "date_created": date_created, "date_modified": date_modified},
+            {"key": "ATTACHED_FILES_CORPUS_DIRECTORY", "value": "files/attached/", "public": True,
+             "date_created": date_created, "date_modified": date_modified},
+            {"key": "ATTACHED_FILES_PREPROCESSOR", "value": "", "public": True, "date_created": date_created,
+             "date_modified": date_modified},
+            {"key": "REDIS_BROKER_URL", "value": "redis://localhost:6379/0", "public": False,
+             "date_created": date_created, "date_modified": date_modified},
+            {"key": "REDIS_TASK_SERIALIZER", "value": "json", "public": False, "date_created": date_created,
+             "date_modified": date_modified},
+            {"key": "REDIS_RESULT_SERIALIZER", "value": "json", "public": False, "date_created": date_created,
+             "date_modified": date_modified},
+            {"key": "REDIS_ACCEPT_CONTENT", "value": "[\"json\"]", "public": False, "date_created": date_created,
+             "date_modified": date_modified},
+            {"key": "MAX_MILLIS_PER_FILE_THRESHOLD", "value": "3.0", "public": False, "date_created": date_created,
+             "date_modified": date_modified}
         ]
     )
 
