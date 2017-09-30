9eca26cb9cf06af745c8552c56a8b06f50e28456
David Cuellar <iidacii@gmail.com>
2017-09-30 18:17:51 -0500

    #72 : fixing auto-resize issue with ui-grid and adding file_store_path config entry.

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,7 +1,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('ThreatKB', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ngSanitize', 'ui.select', 'ngTagsInput',
     'angular-growl', 'angular-toArrayFilter', 'ui.codemirror', 'ngFileUpload', 'ngFileSaver', 'ngPassword',
-    'ngMessages', 'blockUI', 'ui.grid'])
+    'ngMessages', 'blockUI', 'ui.grid', 'ui.grid.autoResize'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/', {
