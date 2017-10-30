8c9f3b812d82eabc22583c95093a1a812669e436
danny <danny@vantagepoint.co>
2017-10-29 23:27:01 -0500

    container support

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ubuntu:14.04
+
+ADD . /opt/inquestkb
+
+
+RUN apt-get update
+RUN apt-get install -y python2.7 python-pip npm libffi-dev libssl-dev mysql-client libmysqlclient-dev python-dev libpython-dev
+RUN pip install virtualenv
+RUN npm install -g bower
+RUN ln -s /usr/bin/nodejs /usr/bin/node
+
+WORKDIR /opt/inquestkb
+
+RUN virtualenv env
+RUN env/bin/pip install -r requirements.txt
+RUN bower install --allow-root
+RUN chmod 744 docker-entrypoint.sh
+RUN chmod 744 wait-for-it.sh
+
+CMD ["/opt/inquestkb/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+
+env
+
+cd /opt/threatkb
+./wait-for-it.sh db:3306 -- echo "db is up"
+env/bin/python manage.py db upgrade
+
+num_users=`echo "select count(*) from kb_users;" | mysql -u ${SQL_USERNAME} -p"${SQL_PASSWORD}" -h ${SQL_HOST} ${SQL_DATABASE} | sed 's/[^0-9]//g'`
+if [ $num_users -lt 1 ]; then
+  PASSWORD=`env/bin/python hash_pass.py ${THREATKB_PASS}`
+  mysql -u ${SQL_USERNAME} -p"${SQL_PASSWORD}" ${SQL_DATABASE} -h ${SQL_HOST} -e "insert into kb_users (email,password,admin,active) values (\"${THREATKB_USER}\", \"${PASSWORD}\", 1, 1);"
+fi
+
+env/bin/python run.py --listen-on 0.0.0.0
