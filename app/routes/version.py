2b102853963f5dcc5d4cf020e5e14192718382df
danny <danny@vantagepoint.co>
2017-11-20 17:59:54 -0600

    #158 in case the version file isn't available return something

--- app/routes/version.py
+++ app/routes/version.py
@@ -9,6 +9,9 @@ import json
 def get_version():
     """Returns the version of ThreatKB.
     Return: version number"""
-    version = [v.strip() for v in open("version", "r").readlines()]
+    try:
+        version = [v.strip() for v in open("version", "r").readlines()]
+    except:
+        version = ["unavailable", "unavailable", "unavailable"]
     return Response(json.dumps({"version": version[0], "version_email": version[1], "version_date": version[2][:-6]}),
                     mimetype='application/json')
