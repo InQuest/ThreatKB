e1c7f49ade4ef0713af1709f3c5d2aa7aed93dc2
deadbits <adam@deadbits.org>
2018-02-17 14:22:19 +0000

    fix install script so if one commands fails the install process doesnt continue

--- install.sh
+++ install.sh
@@ -1,4 +1,27 @@
+#!/bin/bash
+# Bootstrap script to setup environment for ThreatKB
+
 virtualenv flask
+if [ $? -ne 0]; then
+    echo "error: failed to setup virtual environemtn!"
+    exit 1
+fi
+
 flask/bin/pip install -r requirements.txt
+if [ $? -ne 0 ]; then
+    echo "error: failed to install python requirements.txt"
+    exit 1
+fi
+
 flask/bin/python manage.py db upgrade
+if [ $? -ne 0 ]; then
+    echo "error: failed to run 'manage.py db upgrade'"
+    exit 1
+fi
+
 bower install
+if [ $? -ne 0 ]; then
+    echo "error: failed to install bower requirements!"
+    exit 1
+fi
+
