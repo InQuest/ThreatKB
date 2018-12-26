dc23d4e832865db51c056f1e3024835518a0fc46
danny <danny@vantagepoint.co>
2018-12-26 12:38:53 -0600

    Set focus on first input when opening edit/add modal

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -436,7 +436,22 @@ angular.module('ThreatKB').directive("formatDate", function () {
             })
         }
     }
-})
+});
+
+angular.module('ThreatKB').directive('focus', function ($timeout, $parse) {
+    return {
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.focus);
+            scope.$watch(model, function (value) {
+                if (value === true) {
+                    $timeout(function () {
+                        element[0].focus();
+                    });
+                }
+            });
+        }
+    };
+});
 
 angular.module('ThreatKB').config(function (blockUIConfig) {
     // Tell the blockUI service to ignore certain requests
