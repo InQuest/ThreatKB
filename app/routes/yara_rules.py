d3c6573b4d9e1e4bc13501970ca3972929f6b2eb
danny <danny@vantagepoint.co>
2019-02-01 10:29:46 -0600

    Fix uniqueness bug

--- app/routes/yara_rules.py
+++ app/routes/yara_rules.py
@@ -156,7 +156,7 @@ def create_yara_rule():
 
     unique_rule_name_enforcement = Cfg_settings.get_setting("ENFORCE_UNIQUE_YARA_RULE_NAMES")
     if unique_rule_name_enforcement and distutils.util.strtobool(unique_rule_name_enforcement):
-        if db.session.query(yara_rule.Yara_rule).filter(yara_rule.Yara_rule.name == request.json['name']):
+        if db.session.query(yara_rule.Yara_rule).filter(yara_rule.Yara_rule.name == request.json['name']).first():
             raise Exception("You cannot save two rules with the same name.")
 
     compile_on_save = Cfg_settings.get_setting("COMPILE_YARA_RULE_ON_SAVE")
@@ -256,7 +256,7 @@ def update_yara_rule(id):
 
     unique_rule_name_enforcement = Cfg_settings.get_setting("ENFORCE_UNIQUE_YARA_RULE_NAMES")
     if unique_rule_name_enforcement and distutils.util.strtobool(unique_rule_name_enforcement):
-        if db.session.query(yara_rule.Yara_rule).filter(yara_rule.Yara_rule.name == request.json['name']):
+        if db.session.query(yara_rule.Yara_rule).filter(yara_rule.Yara_rule.name == request.json['name']).first():
             raise Exception("You cannot save two rules with the same name.")
 
     compile_on_save = Cfg_settings.get_setting("COMPILE_YARA_RULE_ON_SAVE")
