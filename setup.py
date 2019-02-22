ac6e896cb15b27192a711f6866e70b5aeda3213a
Lee Bontecou <lee.bontecou@inquest.net>
2019-02-22 15:32:08 -0600

    testing version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='threatkb',
-    version='0.1.0',
+    version='0.1.1',
     description='Knowledge base workflow management for Yara rules and C2 artifacts (IP, DNS, SSL)',
 
     url='https://github.com/InQuest/ThreatKB',
