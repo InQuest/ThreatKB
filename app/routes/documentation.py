bcbb5f2e8251fecea24d9c63b3faede4d75bbc4b
danny <danny@vantagepoint.co>
2020-04-05 22:56:47 -0500

    comment out documentation

--- app/routes/documentation.py
+++ app/routes/documentation.py
@@ -1,8 +1,7 @@
 from app import app, auto
 
-
-@app.route('/ThreatKB/documentation', methods=["GET"])
-def documentation():
-    """Generate and return API documentation
-    Return: API documentation HTML string"""
-    return auto.html(template="autodoc_threatkb.html")
+# @app.route('/ThreatKB/documentation', methods=["GET"])
+# def documentation():
+#     """Generate and return API documentation
+#     Return: API documentation HTML string"""
+#     return auto.html(template="autodoc_threatkb.html")
